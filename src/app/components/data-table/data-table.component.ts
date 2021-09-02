795705efcb5adcde61207a621325c8992a30a24d
Paul Joy <pjoyjr@gmail.com>
2021-09-02 17:18:10 -0400

    added some sorting

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -1,4 +1,4 @@
-import { Component,  OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component,  OnInit, Input, Output, EventEmitter, ComponentFactoryResolver } from '@angular/core';
 import { Transaction } from  '../../Transaction';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
@@ -12,11 +12,15 @@ import { Subscription } from 'rxjs';
 export class DataTableComponent implements OnInit {
   @Input() transactions: Transaction[] = [];
   searchWord: any | String = "";
+  filterName: any | String ="";
+  filterAsc: boolean = true;
+  sortSetting: [string, boolean] = ["", true];
   
   showEditTransaction: boolean = false;
   subscription: any | Subscription;
   @Output() onEditTransaction: EventEmitter<Transaction> = new EventEmitter();
   @Output() onDeleteTransaction: EventEmitter<Transaction> = new EventEmitter();
+  @Output() onFilterToggle: EventEmitter<Transaction> = new EventEmitter();
 
   constructor(private uiService:UiService) {
     this.subscription = this.uiService.onToggle().subscribe((value) => (this.showEditTransaction = value)); }
@@ -31,5 +35,15 @@ export class DataTableComponent implements OnInit {
   onEdit(transaction: Transaction): void {
     this.onEditTransaction.emit(transaction);
   }
-
+  
+  onToggle(sortWord: string){
+    if (this.sortSetting[0] != sortWord){
+      this.sortSetting[0] = sortWord;
+      this.sortSetting[1] = true;
+    } else {
+      this.sortSetting[1] = !this.sortSetting[1];
+    }
+    console.log(this.sortSetting);
+    this.onFilterToggle.emit();
+  }
 }
